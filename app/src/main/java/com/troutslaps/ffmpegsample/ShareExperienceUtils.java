cfa1ce2a6e4243b8d70c1010f6306dca5c15dd67
Mai Cervantes <kikiam@gmail.com>
2017-04-21 11:47:09 +0800

    ShareExperienceUtils.java - added methods for drawing text for video ending

--- app/src/main/java/com/troutslaps/ffmpegsample/ShareExperienceUtils.java
+++ app/src/main/java/com/troutslaps/ffmpegsample/ShareExperienceUtils.java
@@ -8,6 +8,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -26,9 +27,12 @@ import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+
+import static android.R.attr.bitmap;
 
 /**
  * Created by maicervantes on 05/04/2017.
@@ -40,6 +44,7 @@ public class ShareExperienceUtils {
     public static final int IMAGE_WIDTH = 640;
     public static final int IMAGE_HEIGHT = 640;
     private static final float TEXT_SIZE_GOODWALL = 28.0f;
+    private static final float TEXT_SIZE_GOODWALL_WATERMARK = 30.0f;
     private static final float TEXT_SIZE_MADE_WITH = 20.0f;
     private static final float TEXT_SIZE_LOCATION = 24.0f;
     private static final float TEXT_SIZE_TITLE = 40.0f;
@@ -53,9 +58,17 @@ public class ShareExperienceUtils {
     private static final float LINE_DECOR_HEIGHT = 1.0f;
     private static final int LOGO_TOP_LINE_MARGIN = 12;
     private static final int LOGO_BOTTOM_LINE_MARGIN = 36;
+    private static final int FOLLOW_MY_STORY_MARGIN = 60;
+    private static final int GOODWALL_WATERMARK_MARGIN = 32;
 
-    private static final int GRADIENT_START_COLOR = 0x00FFFFFF;
-    private static final int GRADIENT_END_COLOR = 0xFFFFFFFF;
+    private static final int GRADIENT_START_COLOR = 0xB3000000;
+    private static final int GRADIENT_END_COLOR = 0x00000000;
+
+
+
+    private static int PROFILE_PHOTO_MARGIN_TOP = 160;
+
+    private static int VIDEO_WATERMARK_WIDTH = 300;
 
     static String goodwall;
     static String madeWith;
@@ -77,6 +90,72 @@ public class ShareExperienceUtils {
         return "file://" + file.getAbsolutePath();
     }
 
+    public static void prepareVideoInfo(String
+            path, Context context, String title, String location) throws IOException {
+
+        Bitmap transparentBitmap = Bitmap.createBitmap(IMAGE_WIDTH, IMAGE_HEIGHT, Bitmap.Config
+                .ARGB_8888);
+        File file = new File(path);
+        OutputStream os = new FileOutputStream(file);
+        ShareExperienceUtils.decorateImageForVideo(context, transparentBitmap, title, location);
+        transparentBitmap.compress(Bitmap.CompressFormat.PNG, 0, os);
+        os.flush();
+        os.close();
+
+    }
+
+    @NonNull
+    public static void prepareVideoEndingWatermark(Context context, Bitmap profilePhoto, String
+            userDisplayName, int imageSize, String path) throws IOException {
+        followMyStory = "Follow my story on".toUpperCase();
+        Bitmap transparentBitmap = Bitmap.createBitmap(imageSize, imageSize, Bitmap.Config
+                .ARGB_8888);
+        Canvas canvas = new Canvas(transparentBitmap);
+        drawProfilePhoto(canvas, profilePhoto);
+
+        TextPaint userDisplayNameTextPaint = new BitmapUtils.PaintBuilder().withTextColor
+                (TEXT_COLOR).withTextSize(TEXT_SIZE_GOODWALL).withTypeface(TypefaceKarla
+                .getTypefaceBold(context)).build();
+
+        TextPaint followMyStoryTextPaint = new BitmapUtils.PaintBuilder().withTextColor
+                (TEXT_COLOR).withTextSize(TEXT_SIZE_GOODWALL).withTypeface(TypefaceKarla
+                .getRegularTypeface(context)).build();
+
+        TextPaint goodwallTextPaint = new BitmapUtils.PaintBuilder().withTextColor
+                (TEXT_COLOR).withTextSize(TEXT_SIZE_GOODWALL_WATERMARK).withTypeface(TypefaceKarla
+                .getTypefaceBold(context)).build();
+
+        int userDisplayNameMargin = PROFILE_PHOTO_MARGIN_TOP + profilePhoto.getHeight() + PADDING;
+        BitmapUtils.TextLayoutInfo userInfo = drawUserDisplayName(canvas,
+                userDisplayNameMargin,
+                userDisplayNameTextPaint,
+                userDisplayName.toUpperCase());
+
+        int followMargin = userInfo.getRectPosition().bottom + FOLLOW_MY_STORY_MARGIN;
+        BitmapUtils.TextLayoutInfo followInfo = drawFollowMyStory(canvas, followMargin,
+                followMyStoryTextPaint, followMyStory);
+
+        int goodwallMargin = followInfo.getRectPosition().bottom + GOODWALL_WATERMARK_MARGIN;
+        int horizontalMargin = 60/2; // icon width = 50, padding = 10 based on sketch
+        BitmapUtils.TextLayoutInfo goodwallWatermarkTextInfo = drawGoodwallWatermarkText(
+                canvas, goodwallTextPaint, goodwallMargin, horizontalMargin, goodwall);
+
+        Bitmap logo = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(context.getResources
+                        (), R.drawable.alex_penguin_purple), 50, 50, true);
+
+        int leftMargin = goodwallWatermarkTextInfo.getRectPosition().left + 5;
+        int topMargin = followInfo.getRectPosition().bottom + 26;
+        drawPenguinLogo(canvas, logo, leftMargin, topMargin);
+        File file = new File(path);
+        OutputStream os = new FileOutputStream(file);
+        transparentBitmap.compress(Bitmap.CompressFormat.PNG, 0, os);
+        os.flush();
+        os.close();
+
+    }
+
+
+
 
     /*
      * convenience method for drawing text on Achievement photo
@@ -85,6 +164,27 @@ public class ShareExperienceUtils {
                                                 String location) {
         Canvas canvas = new Canvas(bitmap);
 
+        decorateImage(canvas, context, title, location);
+
+    }
+
+
+    private static void decorateImageForVideo(Context context, Bitmap bitmap, String title,
+                                                String location) {
+        Canvas canvas = new Canvas(bitmap);
+//        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//        paint.setColor(context.getResources().getColor(android.R.color.white));
+//        paint.setStyle(Paint.Style.FILL);
+//        paint.setAlpha(10);
+//        canvas.drawPaint(paint);
+
+        decorateImage(canvas, context, title, location);
+
+    }
+
+    private static void decorateImage(Canvas canvas, Context context, String title, String
+            location)
+    {
         goodwall = "GOODWALL";
         madeWith = "MADE WITH";
 
@@ -102,7 +202,6 @@ public class ShareExperienceUtils {
 
         // draw the location, relative to the title
         drawLocation(context, location, canvas, titleLayout);
-
     }
 
     private static BitmapUtils.TextLayoutInfo drawGoodwallLogo(Context context, Canvas canvas) {
@@ -239,4 +338,70 @@ public class ShareExperienceUtils {
 
         BitmapUtils.drawTextOnCanvas(canvas, locationLayout);
     }
+
+    private static void drawProfilePhoto(Canvas canvas, Bitmap profilePhoto) {
+        float xPos = (canvas.getWidth() - profilePhoto.getWidth()) / 2;
+        float yPos = PROFILE_PHOTO_MARGIN_TOP;
+        canvas.drawBitmap(profilePhoto, xPos, yPos, null);
+
+    }
+
+    private static BitmapUtils.TextLayoutInfo drawUserDisplayName(Canvas canvas, int
+            userDisplayNameMargin, TextPaint userDisplayNameTextPaint, String userDisplayName) {
+
+        BitmapUtils.TextLayoutInfo userDisplayNameLayout = new BitmapUtils.LayoutBuilder().drawText
+                (userDisplayName)
+                .usingTextPaint(userDisplayNameTextPaint)
+                .fitToWidth(VIDEO_WATERMARK_WIDTH)
+                .onCanvas(canvas)
+                .withVerticalMargin(userDisplayNameMargin)
+                .withHorizontalMargin(0)
+                .placeOnCenter()
+                .withAlignment(Layout.Alignment.ALIGN_CENTER)
+                .build();
+
+        BitmapUtils.drawTextOnCanvas(canvas, userDisplayNameLayout);
+        return userDisplayNameLayout;
+    }
+
+    private static BitmapUtils.TextLayoutInfo drawFollowMyStory(Canvas canvas, int
+            followMargin, TextPaint
+            followMyStoryTextPaint, String followMyStory) {
+
+        BitmapUtils.TextLayoutInfo followMyStoryLayout = new BitmapUtils.LayoutBuilder().drawText
+                (followMyStory)
+                .usingTextPaint(followMyStoryTextPaint)
+                .fitToWidth(VIDEO_WATERMARK_WIDTH)
+                .onCanvas(canvas)
+                .withVerticalMargin(followMargin)
+                .withHorizontalMargin(0)
+                .placeOnCenter()
+                .withAlignment(Layout.Alignment.ALIGN_CENTER)
+                .build();
+        BitmapUtils.drawTextOnCanvas(canvas, followMyStoryLayout);
+        return followMyStoryLayout;
+    }
+
+    private static BitmapUtils.TextLayoutInfo drawGoodwallWatermarkText(Canvas canvas, TextPaint
+            goodwallTextPaint, int topMargin, int horizontalMargin, String goodwall) {
+
+        BitmapUtils.TextLayoutInfo goodwallWatermarkLayout = new BitmapUtils.LayoutBuilder()
+                .drawText(goodwall)
+                .usingTextPaint(goodwallTextPaint)
+                .fitToWidth(VIDEO_WATERMARK_WIDTH)
+                .onCanvas(canvas)
+                .withVerticalMargin(topMargin)
+                .withHorizontalMargin(horizontalMargin)
+                .placeOnCenter()
+                .withAlignment(Layout.Alignment.ALIGN_CENTER)
+                .build();
+        BitmapUtils.drawTextOnCanvas(canvas, goodwallWatermarkLayout);
+        return goodwallWatermarkLayout;
+
+    }
+
+    private static void drawPenguinLogo(Canvas canvas, Bitmap logo, int xPos, int yPos) {
+        canvas.drawBitmap(logo, xPos, yPos, null);
+    }
+
 }
